9abe361334e5e92fd2a0554cd4f02f309cb63d37
XiXnice <kuzminov399@yandex.ru>
2024-04-11 18:08:55 +0300

    admin start

--- reference/netology_pd_diplom/netology_pd_diplom/settings.py
+++ reference/netology_pd_diplom/netology_pd_diplom/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'netology_pd_diplom.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'diplom_db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
-
-
 }
 
 # Password validation
